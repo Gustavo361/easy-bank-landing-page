25b3e2af859f40793c44571f94332de7b740d3a4
Gustavo361 <gustavofaustino361@gmail.com>
2024-02-17 14:16:41 -0300

    fixing new bugs

--- js/initial.js
+++ js/initial.js
@@ -1,7 +1,6 @@
 document.getElementById('logoutButton').addEventListener('click', () => {
-    fetch('https://easy-bank-ui.onrender.com/logout', {
-        method: 'GET',
-    })
+    fetch('https://easy-bank-ui.onrender.com/logout')
+    // fetch('https:localhost:3000/logout')
     .then(response => {
         if (!response.ok) {
             throw new Error('Erro ao fazer logout.');
@@ -10,7 +9,7 @@ document.getElementById('logoutButton').addEventListener('click', () => {
     })
     .then(data => {
         console.log(data.message)
-        window.location.href = data.redirectRoute
+        window.location.href = data.redirectRoute 
     })
     .catch(error => {
         console.error('Erro durante a solicitação de logout:', error);
