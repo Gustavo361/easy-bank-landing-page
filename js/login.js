2e90372a0aa116f85028e7ca7a204db01cc91459
Gustavo361 <gustavofaustino361@gmail.com>
2024-02-20 10:09:03 -0300

    correcting routes

--- js/login.js
+++ js/login.js
@@ -1,19 +1,38 @@
-document.getElementById('logoutButton').addEventListener('click', () => {
-    fetch('https://easy-bank-server.onrender.com/logout', {
+const dataLoginAccount = document.querySelector('[data-login-account]')
+const dataInputUserEmail = document.querySelector('[data-input-user-email]')
+const dataInputUserPassword = document.querySelector('[data-input-user-password]')
+
+dataLoginAccount.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const userEmail = dataInputUserEmail.value
+    const userPassword = dataInputUserPassword.value
+
+    const data = {
+        userEmail: userEmail,
+        userPassword: userPassword,
+    }
+
+    fetch('https://easy-bank-server.onrender.com/login', {
         method: 'POST',
-        credentials: 'include', // Include credentials for cross-origin requests
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro ao fazer logout.');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data.message);
-        window.location.href = data.redirectRoute;
-    })
-    .catch(error => {
-        console.error('Erro durante a solicitação de logout:', error);
-    });
-});
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (!data.ok) {
+                window.alert('Usuário não encontrado.');
+            } else {
+                const dynamicRoute = data.redirectRoute || 'initial.html';
+                window.location.href = dynamicRoute;
+                console.log('Redirected successfully');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Ocorreu um erro ao fazer login.');
+        });
+})
